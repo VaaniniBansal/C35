56f833427cf52a4512fe3452e31d5f7b69e24a23
VaaniniBansal <76036181+VaaniniBansal@users.noreply.github.com>
2021-05-14 17:21:22 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,45 @@
+var ball1;
+var database;
+
+function setup(){
+    createCanvas(500,500);
+    database=firebase.database();
+    ball1 = createSprite(250,250,10,10);
+    ball1.shapeColor = "red";
+    var ballPosition= database.ref('ball/position');
+    ballPosition.on("value",readPosition);
+}
+
+function draw(){
+    background("white");
+
+    if(keyDown(LEFT_ARROW)){
+        writePosition(-1,0);
+    }
+    else if(keyDown(RIGHT_ARROW)){
+        writePosition(1,0);
+    }
+    else if(keyDown(UP_ARROW)){
+        writePosition(0,-1);
+    }
+    else if(keyDown(DOWN_ARROW)){
+        writePosition(0,+1);
+    }
+
+    drawSprites();
+}
+
+
+function writePosition(x,y){
+    database.ref('ball/position').set({
+        'x': position.x+x,
+        'y': position.y+y
+    })
+  
+}
+
+function readPosition(data){
+     position= data.val()
+     ball1.x= position.x;
+     ball1.y= position.y;
+}
